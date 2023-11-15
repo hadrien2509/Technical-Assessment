63ca8ae14d27a733f70b710bfc8985783d5bfdd7
hadrien <hadrien2509@gmail.com>
2023-11-15 14:55:43 +0100

    doing more tests

--- src/AddressBookTest.java
+++ src/AddressBookTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AddressBookTest {
     @Test
     void OldestPerson() {
-        AddressBook addressBook = new AddressBook("files/AddressBook.txt");
+        AddressBook addressBook = new AddressBook("files/test.txt");
         Person oldestPerson = addressBook.OldestPerson();
 
         assertEquals("Wes Jackson", oldestPerson.getName());
@@ -12,7 +12,7 @@ public class AddressBookTest {
 
     @Test
     void NullGenderCount() {
-        AddressBook addressBook = new AddressBook("files/AddressBook.txt");
+        AddressBook addressBook = new AddressBook("files/test.txt");
         int GenderCount = addressBook.GenderCount(null);
 
         assertEquals(-1, GenderCount);
@@ -20,7 +20,7 @@ public class AddressBookTest {
 
     @Test
     void FemaleCount() {
-        AddressBook addressBook = new AddressBook("files/AddressBook.txt");
+        AddressBook addressBook = new AddressBook("files/test.txt");
         int GenderCount = addressBook.GenderCount("Female");
 
         assertEquals(2, GenderCount);
@@ -28,9 +28,27 @@ public class AddressBookTest {
 
     @Test
     void AgeComparison() {
-        AddressBook addressBook = new AddressBook("files/AddressBook.txt");
+        AddressBook addressBook = new AddressBook("files/test.txt");
         long AgeComparison = addressBook.AgeComparison("Bill McKnight", "Paul Robinson");
 
         assertEquals(2862, AgeComparison);
+    }
+
+    //For a leap year
+    @Test
+    void AgeComparisonLeapYear() {
+        AddressBook addressBook = new AddressBook("files/test.txt");
+        long AgeComparison = addressBook.AgeComparison("Eliott Leap", "Elia Leap");
+
+        assertEquals(2, AgeComparison);
+    }
+
+    //For not a leap year
+    @Test
+    void AgeComparisonNoLeapYear() {
+        AddressBook addressBook = new AddressBook("files/test.txt");
+        long AgeComparison = addressBook.AgeComparison("Eliott Noleap", "Elia Noleap");
+
+        assertEquals(1, AgeComparison);
     }
 }
